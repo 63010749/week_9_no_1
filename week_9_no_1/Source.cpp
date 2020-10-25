ed8530af35b67c9bbe1a2f8860bf27203c1a9c83
pawida sattayamuk <TangTana@hotmail.com>
2020-10-25 16:42:29 +0700

    commit1

--- week_9_no_1/Source.cpp
+++ week_9_no_1/Source.cpp
@@ -5,7 +5,7 @@ int thinktime(int min,int hour) {
 		if (min1 > 0) {
 			min1 = 1;
 		}
-		if (min1 = 0) {
+		if (min1 == 0) {
 			min1 = 0;
 		}
 		hour = hour + (min / 60) + min1;
@@ -21,37 +21,45 @@ int thinkcost(int h) {
 int main() {
 	
 	int shopping, hour, minute, cost;
-	scanf("%d %d %d", &shopping, &hour, &minute);
-
+	printf("Shopping : ");
+	scanf("%d", &shopping);
+	printf("Hour : ");
+	scanf("%d", &hour);
+	printf("Minute : ");
+	scanf("%d", &minute);
+	int b ;
+	
 	if (shopping < 0 || hour < 0 || minute < 0) {
 		printf("It can't be negative");
-	}
-	
-	if (shopping > 0 ) {
-		
-		if (shopping < 1000) {
-			hour = thinktime(minute, hour) - 1;
-		}
-		
-		if (shopping >= 1000) {
-			hour = thinktime(minute, hour) - 4;
-		}
-	}
-	
-	cost = thinkcost(hour);
-	
-	if (cost < 0) {
-		printf("cost = 0");
+		b = 0;
 	}
 
 	else {
-		if (cost >= 160) {
-			printf("cost = 150");
+		if (shopping > 0 ) {
+		
+			if (shopping < 1000) {
+				hour = thinktime(minute, hour) - 1;
+			}
+		
+			if (shopping >= 1000) {
+				hour = thinktime(minute, hour) - 4;
+			}
 		}
+	
+		cost = thinkcost(hour);
+	
+		if (cost < 0) {
+			printf("Parking Cost = 0");
+		}
+
 		else {
-			printf("cost = %d", cost);
+			if (cost >= 160) {
+				printf("Parking Cost = 150");
+			}
+			else {
+				printf("Parking Cost = %d", cost);
+			}
 		}
 	}
-
 	return 0;
 }
